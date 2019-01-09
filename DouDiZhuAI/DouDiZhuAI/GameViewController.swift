4568521145af12732254ceff158456f9e46c6304
YunkaiWang <yunkaiwang1996@gmail.com>
2019-01-08 23:14:24 -0500

    update progress, add images to folder, and initial template of login screen

--- DouDiZhuAI/DouDiZhuAI/GameViewController.swift
+++ DouDiZhuAI/DouDiZhuAI/GameViewController.swift
@@ -33,6 +33,7 @@ class GameViewController: UIViewController, UITextFieldDelegate  {
         }
         
         nameInput.delegate = self
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:))))
     }
 
     override var shouldAutorotate: Bool {
@@ -57,5 +58,22 @@ class GameViewController: UIViewController, UITextFieldDelegate  {
         return true
     }
     
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        animateViewMoving(up: true, moveValue: 100)
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        animateViewMoving(up: false, moveValue: 100)
+    }
+    
+    func animateViewMoving (up:Bool, moveValue :CGFloat){
+        let movementDuration:TimeInterval = 0.3
+        let movement:CGFloat = ( up ? -moveValue : moveValue)
+        UIView.beginAnimations( "animateView", context: nil)
+        UIView.setAnimationBeginsFromCurrentState(true)
+        UIView.setAnimationDuration(movementDuration )
+        self.view.frame = self.view.frame.offsetBy(dx: 0, dy: movement)
+        UIView.commitAnimations()
+    }
 }
 
